7f39d3c7c2d10e821f5a971fc2402acb1e0f032d
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-29 17:21:16 -0300

    style: add style button

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
 export const Container = styled.button`
-    
+    height: 50px;
+    border-radius: 10px;
+    border: 1px solid ${({ theme }) => theme.buttonPrimary};
+    background-color: ${({ theme }) => theme.buttonPrimary};
+    color: ${({ theme }) => theme.white};
+    font-weight: bold;
+    font-size: 18px;
+    cursor: pointer;
 `
