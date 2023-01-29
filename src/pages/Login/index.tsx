e6060b63b4beb1f7920765c8517484e8c2f481da
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-29 19:28:50 -0300

    feat: add verify form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,19 +1,50 @@
+import { type FieldErrorsImpl, useForm } from 'react-hook-form'
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
+import { type FormData } from '../../interface'
 import { Container, Title, Form, Navigation } from './style'
 
 export const Login = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
+
+  const sendForm = (data: any) => {
+    console.log(data)
+    console.log(errors)
+  }
+
+  function validateFields (name: 'email' | 'password', erros: Partial<FieldErrorsImpl<FormData>>) {
+    if (erros[name] == null || erros[name] === undefined) { return '' }
+
+    if (erros?.email?.type === 'required') {
+      return 'Email is required'
+    }
+
+    if (erros?.password?.type === 'required') {
+      return 'Password is required'
+    }
+
+    return ''
+  }
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit(sendForm)}>
         <Title>Sign in</Title>
         <Input
           text="Email"
           type='email'
+          name="email"
+          validation={{ required: true }}
+          register={register}
+          message={validateFields('email', errors)}
         />
         <Input
           text="Password"
           type="password"
+          name="password"
+          validation={{ required: true }}
+          register={register}
+          message={validateFields('password', errors)}
         />
         <Button
           text="Entrar"
