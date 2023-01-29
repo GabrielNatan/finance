e854805ebd5a8b917b1c86e2e64a5a33baf98c28
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-29 00:19:21 -0300

    feat: create hook useTheme

---
+++ src/hooks/useTheme.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export const useTheme = () =>{
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+    function toggleTheme(){
+        setIsDarkTheme(!isDarkTheme)
+    }
+
+    return{
+        toggleTheme,
+        isDarkTheme
+    }
+}
