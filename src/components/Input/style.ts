5fd47c7357c08110fe0233d3075ee79ac4cf3ee4
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-29 17:11:00 -0300

    style: add style in Input component

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -1,11 +1,17 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-
+    width: 100%;
+    display: flex;
+    flex-direction: column;
 `
 
 export const InputText = styled.input`
+    padding: 5px 10px;
+    height: 50px;
+    border-radius: 10px;
+    border: 1px solid ${({ theme }) => theme.borderColor};
 `
-export const Text = styled.p`
+export const Text = styled.label`
 
 `
