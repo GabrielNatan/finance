ad25570e94be7ba558ab9fd7c9f9c472f78d83e3
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-29 17:12:37 -0300

    feat: create structure for login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,28 @@
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+import { Container, Title, Form, Navigation } from './style'
+
 export const Login = () => {
-  return (<div>Login</div>)
+  return (
+    <Container>
+      <Form>
+        <Title>Sign in</Title>
+        <Input
+          text="Email"
+          type='email'
+        />
+        <Input
+          text="Password"
+          type="password"
+        />
+        <Button
+          text="Entrar"
+          type="submit"
+        />
+        <Navigation to="/">
+          Create account
+        </Navigation>
+      </Form>
+    </Container>
+  )
 }
