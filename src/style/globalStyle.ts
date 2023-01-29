b6346fca622d7b5859336693810b2cb55f842f20
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-28 23:56:43 -0300

    feat: create themeProps

--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -1,12 +1,26 @@
 import  { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
-
-*{
-    margin: 0;
-    padding: 0;
-    font-family: "Poppins";
-    box-sizing: border-box;
+interface themeProps{
+    color:string;
+    background:string;
+    backgroundSecondary:string;
+    borderColor:string;
+    shadowColor:string;
+    buttonPrimary:string;
+    active:string;
+    green:string;
+    green30:string;
+    red:string;
+    red30:string;
 }
+
+export const GlobalStyle = createGlobalStyle<{theme:themeProps}>`
+
+    *{
+        margin: 0;
+        padding: 0;
+        font-family: "Poppins";
+        box-sizing: border-box;
+    }
 
 `
