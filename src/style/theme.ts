37ada9a1a1f223bf4c035cd9889641efa5c45f6e
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-28 23:44:18 -0300

    refactor: separate thema and global style to make the code clean

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,15 +1,3 @@
-import  { createGlobalStyle } from "styled-components";
-
-export const GlobalStyle = createGlobalStyle`
-
-*{
-    margin: 0;
-    padding: 0;
-    font-family: "Poppins";
-    box-sizing: border-box;
-}
-
-`
 
 export const lightTheme = {
     color:"#222222",
