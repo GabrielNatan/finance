d1ff9baf93cad47834d761535475027d5145e179
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-29 19:29:37 -0300

    style: adds style to message errors

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -1,17 +1,26 @@
 import styled from 'styled-components'
 
+interface InputTextProps {
+  message: boolean
+}
+
 export const Container = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
 `
 
-export const InputText = styled.input`
+export const InputText = styled.input<InputTextProps>`
     padding: 5px 10px;
     height: 50px;
     border-radius: 10px;
     border: 1px solid ${({ theme }) => theme.borderColor};
+    border-color: ${({ theme, message }) => message ? theme.red : theme.borderColor};
 `
 export const Text = styled.label`
 
+`
+
+export const MessageError = styled.div`
+    color: ${({ theme }) => theme.red};
 `
