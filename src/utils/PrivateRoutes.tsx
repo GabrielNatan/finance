502c2b0c2585e2eda74605bb339047e58228bd1c
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-29 23:34:50 -0300

    refactor: add token in private routes

--- src/utils/PrivateRoutes.tsx
+++ src/utils/PrivateRoutes.tsx
@@ -1,9 +1,9 @@
 import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
 
 export const PrivateRoutes = () => {
-  const auth = false
-
+  const { token } = useAuth()
   return (
-    auth ? <Outlet/> : <Navigate to="/login"/>
+    token ? <Outlet/> : <Navigate to="/login"/>
   )
 }
