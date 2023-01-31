3c74f66dd41d7f0a6dbe19151d35dfb7a8b8e6e0
gabriel natan <g.souza@grouplinknetwork.com>
2023-01-30 21:02:40 -0300

    feat: create page CreateAccount

---
+++ src/pages/CreateAccount/index.tsx
@@ -0,0 +1,100 @@
+import { type FieldErrorsImpl, useForm } from 'react-hook-form'
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+import { useAuth } from '../../hooks/useAuth'
+import { type FormDataOptions, type FormData } from '../../interface'
+import { Container, Title, Form, Navigation, ContainerInput } from './style'
+import { useNavigate } from 'react-router-dom'
+
+export const CreateAccount = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
+  const { toggleToken } = useAuth()
+  const navigate = useNavigate()
+
+  const sendForm = (data: any) => {
+    toggleToken()
+    navigate('/')
+  }
+
+  function validateFields (name: FormDataOptions, erros: Partial<FieldErrorsImpl<FormData>>) {
+    const object: FormData = { email: '', password: '', lastName: '', confirmPassword: '', name: '' }
+
+    if (erros?.email?.type === 'required') {
+      object.email = 'Email is required'
+    }
+
+    if (erros?.password?.type === 'required') {
+      object.password = 'Password is required'
+    }
+
+    if (erros?.confirmPassword?.type === 'required') {
+      object.confirmPassword = 'Confirm Password is required'
+    }
+
+    if (erros?.name?.type === 'required') {
+      object.name = 'Name is required'
+    }
+
+    if (erros?.lastName?.type === 'required') {
+      object.lastName = 'Last Name is required'
+    }
+    return object
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit(sendForm)}>
+        <Title>Create Account</Title>
+        <ContainerInput>
+          <Input
+            text="Name"
+            type='text'
+            name="name"
+            validation={{ required: true }}
+            register={register}
+            message={validateFields('name', errors)}
+          />
+          <Input
+            text="Last Name"
+            type='text'
+            name="lastName"
+            validation={{ required: true }}
+            register={register}
+            message={validateFields('lastName', errors)}
+          />
+        </ContainerInput>
+        <Input
+          text="Email"
+          type='email'
+          name="email"
+          validation={{ required: true }}
+          register={register}
+          message={validateFields('email', errors)}
+        />
+        <Input
+          text="Password"
+          type="password"
+          name="password"
+          validation={{ required: true }}
+          register={register}
+          message={validateFields('password', errors)}
+        />
+        <Input
+          text="Confrim password"
+          type='password'
+          name="confirmPassword"
+          validation={{ required: true }}
+          register={register}
+          message={validateFields('confirmPassword', errors)}
+        />
+        <Button
+          text="Entrar"
+          type="submit"
+        />
+        <Navigation to="/">
+          Create account
+        </Navigation>
+      </Form>
+    </Container>
+  )
+}
---
+++ src/pages/CreateAccount/style.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Form = styled.form`
+    width: 100%;
+    max-width: 530px;
+    padding: 45px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    
+    @media screen and (min-width: 768px){
+        width: 530px;
+        padding: 40px;    
+        box-shadow: 0px 0px 4px ${({ theme }) => theme.shadowColor};
+        background-color: ${({ theme }) => theme.backgroundSecondary};
+    }
+`
+export const ContainerInput = styled.div`
+    max-width: 100%;
+    display: flex;
+    gap: 20px;
+    justify-content: space-between;
+`
+
+export const Title = styled.h1`
+    color: ${({ theme }) => theme.color};
+    font-size: 25px;
+`
+
+export const Navigation = styled(Link)`
+    color: ${({ theme }) => theme.active};
+    text-decoration: none;
+    text-align: center;
+
+`
