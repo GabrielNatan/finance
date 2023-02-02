a8e89c3db45967e49cd440f7852937ea7dac344f
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-01 21:43:27 -0300

    feat: create structure Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,54 @@
+import { Avatar } from '../../components/Avatar'
+import {
+  Container,
+  Notification,
+  Header,
+  ContAvatar,
+  ContBalance,
+  Text,
+  Money,
+  Image,
+  InputAndOutput,
+  Tab,
+  Main,
+  Left,
+  Right
+} from './style'
+import { FaEye, FaBell } from 'react-icons/fa'
+import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai'
+
 export const Home = () => {
-  return (<div>HOME</div>)
+  return (
+    <Container>
+      <Notification>
+        <FaBell size={20} color="#AAA"/>
+      </Notification>
+      <Header>
+        <ContAvatar>
+            <Avatar
+                 width={60}
+                 height={60}
+                 src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+                 alt="user avatar"
+            />
+        </ContAvatar>
+        <ContBalance>
+          <Text>Balance <Image><FaEye size={20} color="#AAA"/></Image></Text>
+          <Money>U$ 3,700.00 </Money>
+        </ContBalance>
+        <InputAndOutput>
+          <Tab type='success'><AiOutlineArrowDown size={12} color="green"/> U$ 190.00</Tab>
+          <Tab type='error'><AiOutlineArrowUp size={12} color="red"/> U$ 2,014.00</Tab>
+        </InputAndOutput>
+      </Header>
+      <Main>
+        <Left>
+
+        </Left>
+        <Right>
+
+        </Right>
+      </Main>
+    </Container>
+  )
 }
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,107 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    flex: 1;
+    width: 100%;
+    height: 100%;
+`
+
+export const Notification = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    padding: 10px 0;
+`
+export const Header = styled.div`
+    width: 100%;
+    background-color: ${({ theme }) => theme.backgroundSecondary};
+    border-radius: 0 0 60px 60px;
+    padding: 20px 30px 50px 30px;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    box-shadow: 0px 0px 10px ${({ theme }) => theme.shadowColor};
+    @media screen and (min-width: 768px) {
+        border-radius: 10px;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: flex-end;
+        padding: 20px 40px;
+    }
+`
+
+export const ContAvatar = styled.div`
+    display: initial;
+    @media screen and (min-width: 768px) {
+        display: none;
+    }
+`
+
+export const ContBalance = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const Text = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 30px;
+    line-height: 35px;
+    font-weight: 500;
+    @media screen and (min-width: 768px) {
+        font-weight: bold;
+        font-size: 35px;
+        line-height: 55px;
+    }
+`
+
+export const Money = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 40px;
+    line-height: 45px;
+    font-weight: bold;
+    @media screen and (min-width: 768px) {
+        font-weight: 500;
+        font-size: 35px;
+        line-height: 55px;
+    }
+`
+
+export const Image = styled.div`
+
+`
+
+export const InputAndOutput = styled.div`
+   display: none;
+   @media screen and (min-width: 768px) {
+        display: flex;
+        gap: 20px;
+    }
+`
+
+export const Tab = styled.div<{ type: 'success' | 'error' }>`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    padding: 3px 10px;
+    border-radius: 20px;
+    font-size: 12px;
+    font-weight: bold;
+    color: ${({ theme, type }) => type === 'success' ? theme.green : theme.red};
+    background-color: ${({ theme, type }) => type === 'success' ? theme.green30 : theme.red30};
+`
+
+export const Main = styled.div`
+
+`
+
+export const Left = styled.div`
+
+`
+
+export const Right = styled.div`
+
+`
