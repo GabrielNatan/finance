a222fed2ecb241244d8640e1282662a0d96f1c7b
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-02 21:00:30 -0300

    refactor: change Auth Provider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,19 +1,46 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import { db } from '../api/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
 import PropTypes from 'prop-types'
 
-export const AuthContext = createContext({ token: false, toggleToken: () => { console.log('aqui') } })
+interface Customer { displayName: string | null, email: string | null, phoneNumber: string | null, photoURL: string | null, uid: string | null }
+const userElement: Customer = { displayName: '', email: '', phoneNumber: '', photoURL: '', uid: '' }
 
+export const AuthContext = createContext(
+  {
+    token: false,
+    toggleToken: (value: boolean) => { console.log('aqui') },
+    user: userElement
+  }
+)
 AuthContext.displayName = 'Auth'
 
 export const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-  const [token, setToken] = useState(false)
-  const value = {
-    token,
-    toggleToken
+  const abc = localStorage.getItem('user') !== undefined
+  const [token, setToken] = useState(abc)
+  const [user, setUser] = useState<Customer>({ displayName: '', email: '', phoneNumber: '', photoURL: '', uid: '' })
+  const { auth } = db
+
+  function toggleToken (value: boolean) {
+    setToken(value)
   }
 
-  function toggleToken () {
-    setToken(!token)
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user != null) {
+        const currentUser = { displayName: user.displayName, email: user.email, phoneNumber: user.phoneNumber, photoURL: user.photoURL, uid: user.uid }
+        setUser(currentUser)
+        setToken(true)
+      } else {
+        toggleToken(false)
+      }
+    })
+  }, [])
+
+  const value = {
+    token,
+    toggleToken,
+    user
   }
   return (
         <AuthContext.Provider value={{ ...value }}>
