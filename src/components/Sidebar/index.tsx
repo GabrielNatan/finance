5b7596acdd80d7d4610b685370800c7a1be1abbd
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-02 22:53:05 -0300

    refactor: add user in sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -12,18 +12,21 @@ import { AiOutlineSetting } from 'react-icons/ai'
 import { TfiWallet } from 'react-icons/tfi'
 import { GrTransaction } from 'react-icons/gr'
 import { Avatar } from '../Avatar'
+import { useAuth } from '../../hooks/useAuth'
 
 export const Sidebar = () => {
+  const { user } = useAuth()
+  const image = user.photoURL !== null ? user.photoURL : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'
   return (
     <Container>
       <Header>
         <Avatar
           width={90}
           height={90}
-          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-          alt="user avatar"
+          src={image}
+          alt={`${user.displayName !== null ? user.displayName : 'user'} avatar`}
         />
-        <Name>Gabriel Natan</Name>
+        <Name>{user.displayName}</Name>
       </Header>
       <Nav>
         <List>
