c72fb8e1abbaa31d433aa865704ed00525b406f9
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-06 23:18:27 -0300

    feat: add storage

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -13,8 +14,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const firestore = getFirestore(app)
+const storage = getStorage(app)
 const auth = getAuth(app)
 export const db = {
   firestore,
-  auth
+  auth,
+  storage
 }
