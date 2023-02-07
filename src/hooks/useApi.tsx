ad5b9f6cb17041f8b767854a9a54737a5cca19f0
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-06 23:20:33 -0300

    feat: add updateProfile

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,5 +1,6 @@
 import { db } from '../api/firebase'
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 
 export const useApi = () => {
   const { auth } = db
@@ -11,12 +12,31 @@ export const useApi = () => {
   async function createUser (name: string, lastName: string, email: string, password: string) {
     const user = await createUserWithEmailAndPassword(auth, email, password)
     await updateProfile(user.user, {
-      displayName: `${name} ${lastName}`
+      displayName: `${name} ${lastName}`,
+      photoURL: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'
     })
+  }
+
+  async function updateUser (file: File) {
+    const { storage, auth } = db
+    const imageRef = ref(storage, 'perfil/' + file.name)
+    uploadBytesResumable(imageRef, file)
+      .then(async (snapshot) => {
+        await getDownloadURL(snapshot.ref).then(async (url) => {
+          console.log('File available at', url)
+          if (auth.currentUser !== null) {
+            await updateProfile(auth.currentUser, { photoURL: url })
+          }
+        })
+      }).catch((error) => {
+        console.error('Upload failed', error)
+        // ...
+      })
   }
 
   return {
     login,
-    createUser
+    createUser,
+    updateUser
   }
 }
