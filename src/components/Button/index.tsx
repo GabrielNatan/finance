a8d405393a0c32dafd963b1540d18351912d9874
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-06 23:19:25 -0300

    refactor: add on click in button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,13 +6,15 @@ interface ButtonProps {
   type: 'button' | 'submit' | 'reset' | undefined
   children: JSX.Element | string
   disabled: boolean
+  onclick?: () => void
 }
 
-export const Button = ({ type, children, disabled }: ButtonProps) => {
+export const Button = ({ type, children, disabled, onclick }: ButtonProps) => {
   return (
         <Container
           type={type}
           disabled={disabled}
+          onClick={onclick}
         >
             {children}
         </Container>
