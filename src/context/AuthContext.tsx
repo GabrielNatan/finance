500b48746969ce7de42abbb3805d57e42c2e2054
gabriel natan <g.souza@grouplinknetwork.com>
2023-02-06 23:20:15 -0300

    refactor: add activated

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,9 +3,10 @@ import { db } from '../api/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 import PropTypes from 'prop-types'
 import { useLocalStorage } from '../hooks/useLocalStorage'
+import moment from 'moment'
 
-interface Customer { displayName: string | null, email: string | null, phoneNumber: string | null, photoURL: string | null, uid: string | null }
-const userElement: Customer = { displayName: '', email: '', phoneNumber: '', photoURL: '', uid: '' }
+interface Customer { activated: string | undefined, displayName: string | null, email: string | null, phoneNumber: string | null, photoURL: string | null, uid: string | null }
+const userElement: Customer = { activated: '', displayName: '', email: '', phoneNumber: '', photoURL: '', uid: '' }
 
 export const AuthContext = createContext(
   {
@@ -18,7 +19,7 @@ AuthContext.displayName = 'Auth'
 
 export const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   const [token, setToken] = useState(useLocalStorage('user'))
-  const [user, setUser] = useState<Customer>({ displayName: '', email: '', phoneNumber: '', photoURL: '', uid: '' })
+  const [user, setUser] = useState<Customer>({ activated: '', displayName: '', email: '', phoneNumber: '', photoURL: '', uid: '' })
   const { auth } = db
 
   function toggleToken (value: boolean) {
@@ -28,7 +29,8 @@ export const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) =>
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user != null) {
-        const currentUser = { displayName: user.displayName, email: user.email, phoneNumber: user.phoneNumber, photoURL: user.photoURL, uid: user.uid }
+        console.log('Null ', user)
+        const currentUser = { activated: moment(user.metadata.creationTime).fromNow(), displayName: user.displayName, email: user.email, phoneNumber: user.phoneNumber, photoURL: user.photoURL, uid: user.uid }
         setUser(currentUser)
         setToken(true)
       } else {
